2a89aca20a6076e19e3384b4daaad678127dee83
adamkhobba <adamekhobba6@gmail.com>
2025-03-25 03:46:33 +0000

    rm mulit function

--- src/raycasting/projection_wall.c
+++ src/raycasting/projection_wall.c
@@ -6,46 +6,11 @@
 /*   By: akhobba <akhobba@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 01:46:40 by akhobba           #+#    #+#             */
-/*   Updated: 2025/03/25 03:44:37 by akhobba          ###   ########.fr       */
+/*   Updated: 2025/03/25 03:45:46 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-unsigned int	get_pixel_img(t_img *data, int x, int y)
-{
-	return (*(unsigned int *)(data->addr + (y * data->line_len + x
-			* (data->bpp / 8))));
-}
-
-
-t_img *initialize_text(t_data *data, char *path)
-{
-    t_img *text = malloc(sizeof(t_img));
-    printf("path: %s\n", path);
-    text->img = mlx_xpm_file_to_image(data->mlx.instance, path, &text->width, &text->height);
-    if (!text->img)
-    {
-        ft_putstr_fd("Error\nTexture not found\n", 2);
-        exit(1);
-    }
-    text->addr = mlx_get_data_addr(text->img ,&text->bpp , &text->line_len, &text->endian);
-    if (!text->addr)
-    {
-        ft_putstr_fd("Error\nTexture not found2\n", 2);
-        exit(1);
-    }
-    return text;
-}
-
-void init_text(t_data *data)
-{
-    data->textures[0] = initialize_text(data, data->map->no);
-    data->textures[1] = initialize_text(data, data->map->so);
-    data->textures[2] = initialize_text(data, data->map->we);
-    data->textures[3] = initialize_text(data, data->map->ea);
-}
-
 
 void render_projection_walls(t_ray *rays, int num_rays)
 {
