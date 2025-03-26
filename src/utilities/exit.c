c1ca63b758cc8aa689f7914b5c871c4f05ca6406
adamkhobba <adamekhobba6@gmail.com>
2025-03-26 05:23:47 +0000

    free leaks to textures

--- src/utilities/exit.c
+++ src/utilities/exit.c
@@ -6,7 +6,7 @@
 /*   By: akhobba <akhobba@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 01:06:09 by akhobba           #+#    #+#             */
-/*   Updated: 2025/03/25 01:32:11 by akhobba          ###   ########.fr       */
+/*   Updated: 2025/03/26 05:23:22 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ void free_textures(t_img **textures)
 	i = 0;
 	while (i < 4)
 	{
-		// if (textures[i]->img)
-		// 	mlx_destroy_image(get_data()->mlx.instance, textures[i]->img);
-		free(textures[i]->img);
+		mlx_destroy_image(get_data()->mlx.instance, textures[i]->img);
 		free(textures[i]);
 		i++;
 	}
@@ -29,14 +27,14 @@ void free_textures(t_img **textures)
 
 int	close_program(void)
 {
+	free_textures(get_data()->textures);
+	free(get_data()->player);
+	free_elements(get_data()->map);
+	free_memory(get_data()->map);
 	mlx_destroy_image(get_data()->mlx.instance, get_data()->mlx.image.img);
 	mlx_destroy_window(get_data()->mlx.instance, get_data()->mlx.win);
 	mlx_destroy_display(get_data()->mlx.instance);
-	free_textures(get_data()->textures);
-	free(get_data()->player);
 	free(get_data()->mlx.instance);
-	free_elements(get_data()->map);
-	free_memory(get_data()->map);
 	exit(0);
 	return (0);
 }
